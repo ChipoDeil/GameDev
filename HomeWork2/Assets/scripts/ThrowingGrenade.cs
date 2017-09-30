a0950bd06f73935424ea508aafc7135693bad661
yaroslav <bus.yaroslav@gmail.com>
2017-09-30 14:54:33 +0300

    HomeWork2 DONE

---
+++ HomeWork2/Assets/scripts/ThrowingGrenade.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowingGrenade : MonoBehaviour
+{
+    public GameObject grenade;
+    public GameObject camera;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1)) { 
+            if(GameObject.Find("Grenade") != null) { 
+                Debug.Log("Pressed right click.");
+                grenade = GameObject.Find("Grenade");
+                camera = GameObject.Find("Main Camera");
+                Debug.Log(grenade.name);
+                grenade.AddComponent<SphereCollider>();
+                grenade.AddComponent<Rigidbody>();
+                grenade.transform.parent = null;
+                Vector3 lookDir = camera.transform.forward;
+                grenade.GetComponent<Rigidbody>().AddForce(lookDir * 500);
+                grenade.AddComponent<СollisionChecker>();
+            }
+        }
+    }
+}
---
+++ HomeWork2/Assets/scripts/TriggerForOpenTheDoor.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerForOpenTheDoor : MonoBehaviour {
+    private bool _isOpen = false;
+    void OnTriggerEnter(Collider other) {
+        GameObject light = GameObject.Find("DoorLight");
+        Debug.Log("EnteredTriggerForOpenTheDoor");
+        light.GetComponent<Light>().enabled = true;
+        
+    }
+
+    void OnTriggerStay(Collider other) {
+        if (!_isOpen) { 
+            GameObject camera = GameObject.Find("Main Camera");
+            string nameOfObject = camera.GetComponent<LookDirection>().getNameOfObject();
+            if (nameOfObject.Equals("DoorCube") && Input.GetKeyDown(KeyCode.F))
+            {
+                GameObject doorCube = GameObject.Find("Door");
+                doorCube.transform.Rotate(0, 90, 0);
+                _isOpen = true;
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other) {
+        GameObject light = GameObject.Find("DoorLight");
+        Debug.Log("ExitedTriggerForOpenTheDoor");
+        light.GetComponent<Light>().enabled = false;
+        if (_isOpen) {
+            GameObject doorCube = GameObject.Find("Door");
+            doorCube.transform.Rotate(0, -90, 0);
+            _isOpen = false;
+        }
+    }
+}
