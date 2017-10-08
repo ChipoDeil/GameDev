c32a16c83f8c96e044fef0c5c2584c392e3a9ced
Unknown <bus.yaroslav@gmail.com>
2017-10-08 16:09:08 +0300

    HomeWork3 Added
    lol

---
+++ HomeWork3_Animation+Coroutine/Assets/Scripts/DealWithAnimation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class DealWithAnimation : MonoBehaviour {
+    Animator anim;
+    const float _startDelay = 0.5f;
+    const float _endDelay = 5f;
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        StartCoroutine(PlayAnimation());
+	}
+
+    IEnumerator PlayAnimation() {
+        anim.SetBool("Ready", true);
+        float delay = Random.Range(_startDelay, _endDelay);
+        yield return new WaitForSeconds(delay);
+        anim.SetBool("Ready", false);
+
+    }
+}
