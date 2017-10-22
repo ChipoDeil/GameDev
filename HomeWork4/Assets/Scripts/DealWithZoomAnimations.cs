d173524d3613fdd2fe4a1f9a8f9c5677024aed4d
yaroslav <bus.yaroslav@gmail.com>
2017-10-22 17:15:27 +0300

    homework 4 done

---
+++ HomeWork4/Assets/Scripts/DealWithZoomAnimations.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DealWithZoomAnimations : MonoBehaviour {
+
+	private Animator anim;
+    private string zoomIn = "ZoomIn";
+    private string zoomOut = "ZoomOut";
+    int counter = 0;
+	void Start () {
+        anim = this.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Mouse1) && counter > -1) {
+            anim.SetTrigger(zoomOut);
+            counter--;
+        }
+        if (Input.GetKeyDown(KeyCode.Mouse0) && counter < 1) {
+            anim.SetTrigger(zoomIn);
+            counter++;
+        }
+    }
+}
